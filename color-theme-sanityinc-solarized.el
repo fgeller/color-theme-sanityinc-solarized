8561046ad258f0d1e6ce0b09bb1f025ddcae3698
Felix Geller <fgeller@gmail.com>
2012-02-29 12:01:23 +0100

    candidate face should fit the other mode-line faces.

--- color-theme-sanityinc-solarized.el
+++ color-theme-sanityinc-solarized.el
@@ -134,7 +134,7 @@ names to which it refers are bound."
      (anything-bookmarks-su-face ((,class (:foreground ,blue))))
      (anything-buffer-not-saved ((,class (:foreground ,blue))))
      (anything-buffer-saved-out ((,class (:foreground ,blue))))
-     (anything-candidate-number ((,class (:foreground ,blue :background nil))))
+     (anything-candidate-number ((,class (:foreground nil :background nil))))
      (anything-emms-playlist ((,class (:foreground ,blue))))
      (anything-ff-directory ((,class (:foreground ,blue))))
      (anything-ff-executable ((,class (:foreground ,blue))))
